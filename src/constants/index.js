307c2a91644c79a2286e27e737542a5bef564acc
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-10 13:25:00 +0530

    Start working on index

---
+++ src/constants/index.js
@@ -0,0 +1,29 @@
+import {
+    blackImg,
+    blueImg,
+    highlightFirstVideo,
+    highlightFourthVideo,
+    highlightSecondVideo,
+    highlightThirdVideo,
+    whiteImg,
+    yellowImg,
+  } from "../utils";
+  
+  export const navLists = ["Store", "Mac", "iPhone", "Support"];
+  
+  export const hightlightsSlides = [
+    {
+      id: 1,
+      textLists: [
+        "Enter A17 Pro.",
+        "Game‑changing chip.",
+        "Groundbreaking performance.",
+      ],
+      video: highlightFirstVideo,
+      videoDuration: 4,
+    },
+    {
+      id: 2,
+      textLists: ["Titanium.", "So strong. So light. So Pro."],
+      video: highlightSecondVideo,
+      videoDuration: 5,
