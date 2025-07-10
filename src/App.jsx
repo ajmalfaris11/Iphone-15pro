68c10a922f7dd0594a7864f0e8def2fd816cbd7b
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-10 10:38:00 +0530

    Start working on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import Model from './components/Model'
+import Features from './components/Features'
+import HowitWorks from './components/HowitWorks'
+import Footer from './components/Footer'
+import Lenis from '@studio-freight/lenis';
+import { useRef, useEffect } from 'react';
+import * as Sentry from "@sentry/react";
 
-function App() {
-  const [count, setCount] = useState(0)
+  
+const App = ()=> {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  const lenisRef = useRef(null);
 
-export default App
