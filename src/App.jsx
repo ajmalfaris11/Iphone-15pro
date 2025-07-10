0cf8b5840d55271f981e2d36ec7037ed8c3162bb
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-10 10:58:00 +0530

    Set up App features

--- src/App.jsx
+++ src/App.jsx
@@ -15,3 +15,20 @@ const App = ()=> {
 
   const lenisRef = useRef(null);
 
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smooth: true,
+    });
+
+    lenisRef.current = lenis;
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
