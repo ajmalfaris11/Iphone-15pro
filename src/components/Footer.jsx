92fa06efeeac3b6cd82ee974b878d9e69c4fcc2f
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-11 10:37:00 +0530

    Enhance Footer features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,3 +26,17 @@ const Footer = () => {
        <p className='font-semibold text-gray text-xs'>
         Copyright © 2026 Apple-By-Ajmal Inc. All rights reserved.
        </p>
+       <div className='flex'>
+        {
+            footerLinks.map((link , i)=>(
+                <p key={link} className='font-semibold text-gray text-xs'>{link}{' '}
+                {i!==footerLinks.length-1 && <span className='mx-2'> | </span>}</p>
+            ))
+        }
+       </div>
+       </div>
+       </div>
+      
+    </footer>
+  )
+}
