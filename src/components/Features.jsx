3aa8929180521107aa824c0a6886ffbe6d68c189
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-11 08:59:00 +0530

    Refine Features features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -35,3 +35,40 @@ const Features = () => {
             '.g_text', {
                 y: 0,
                 opacity: 1,
+                ease: 'power2.inOut',
+                duration: 1
+            }
+        );     
+    },[]);
+  return (
+    <section className='h-full common-padding
+    bg-zinc overflow-hidden'>
+      <div className='screen-max-width'>
+        <div className='mb-12 w-full'>
+          <h1 id='features_title' className='section-heading'>Explore the full story.</h1>
+        </div>
+        <div className='flex flex-col justify-center items-center overflow-hidden'>
+          <div className='mt-32 mb-24 pl-24'>
+             <h2 className='text-5xl lg:7xl font-semibold'>iPhone.</h2>
+             <h2 className='text-5xl lg:7xl font-semibold'>Forged in titanium.</h2>
+          </div>
+
+          <div className='flex-center flex-col sm:px-10'>
+            <div className='relative h-[50vh] w-full flex items-center'> 
+              <video 
+              playsInline
+              id='exploreVideo'
+              className='w-full h-full object-cover object-center'
+              preload='none'
+              muted
+              autoPlay
+              ref={videoRef}>
+                <source src={exploreVideo} type='video/mp4'/>
+              </video>
+            </div>
+            <div className='flex flex-col w-full relative'>
+               <div className='feature-video-container'>
+                   <div className='overflow-hidden flex-1 h-[50vh]'>
+                     <img src={explore1Img} alt="titanium" className='feature-video g_grow' />
+                   </div>
+                   <div className='overflow-hidden flex-1 h-[50vh]'>
