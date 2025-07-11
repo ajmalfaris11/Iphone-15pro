7838674df75aa61aa830773e3daa66bfdd73358a
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-11 13:50:00 +0530

    Finalize implementation of Highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -30,3 +30,19 @@ const Highlights = () => {
           </h1>
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
+                Watch the film
+                <img src={watchImg} alt="watch" className="ml-2" />
+                </p>
+            <p className="link">
+                Watch the event
+                <img src={rightImg} alt="right" className="ml-2" />
+                </p>
+          </div>
+        </div>
+        <VideoCarousel/>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
