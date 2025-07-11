f357c511af353ea65286a4b85a131554a7486e78
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-11 13:38:00 +0530

    Refine Highlights features

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -14,3 +14,19 @@ const Highlights = () => {
         opacity:1,
         delay:1.5,
         y:0,
+        stagger:0.25
+    })
+  }, []);
+  return (
+    <section
+      id="highlights"
+      className="w-screen common-padding 
+    overflow-hidden h-full bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end md:flex justify-between">
+          <h1 id="title" className="section-heading">
+            Get the highlights
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
