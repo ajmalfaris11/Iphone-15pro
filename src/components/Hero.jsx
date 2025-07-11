2ea5d0e0e8cd4d3c97a79174d09ba870ff706752
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-11 12:11:00 +0530

    Add styles to Hero features

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,3 +32,20 @@ const Hero = () => {
     },[])
   return (
    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col'>
+       <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+       <div className='md:w-10/12 w-9/12'>
+         <video className='pointer-events-none' autoPlay muted loop playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type='video/mp4'/>
+         </video>
+       </div>
+      </div>
+
+      <div id='cta' className='flex flex-col items-center opacity-0 translate-y-20'>
+        <a href="#highlights" className='btn'>Buy</a>
+        <p className='font-normal text-xl'>From $199/month or $999</p>
+      </div>
+   </section>
+  )
+}
+
