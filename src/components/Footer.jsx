e9a0865e1a8dd6ef3548f7f2c23db21b59294bbe
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-11 10:01:00 +0530

    Start working on Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { footerLinks } from '../constants'
+
+const Footer = () => {
+  return (
+    <footer className='py-5 sm:px-10 px-5'>
+      <div className='screen-max-width '>
+       <div>
+        <p className='font-semibold text-gray text-xs'>More ways to shop: {' '}
+            <span className='underline text-blue'>
+              Find an Apple store {' '}
+            </span>
+            Or {' '}
+            <span className='underline text-blue'>
