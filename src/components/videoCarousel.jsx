70919c50dc81f3574d8dec7faa1525e618047058
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-13 16:51:00 +0530

    Design videoCarousel features

--- src/components/videoCarousel.jsx
+++ src/components/videoCarousel.jsx
@@ -68,3 +68,73 @@ const videoCarousel = () => {
                 const progress = Math.ceil(anim.progress() *
               100);
 
+              if(progress !== currntProgress){
+                  currntProgress = progress;
+
+                  gsap.to(videoDivRef.current[videoId],{
+                    width:window.innerWidth < 760 
+                    ? '10vw' 
+                    : window.innerWidth < 1200 ?
+                    '10vw'
+                    : '4vw'
+                  })
+
+                  gsap.to(span[videoId],{
+                    width:`${currntProgress}%`,
+                    backgroundColor:'white'
+                  })
+              }
+            },
+            onComplete:() =>{
+              if(isPlaying){
+                gsap.to(videoDivRef.current[videoId],{
+                  width:'12px'
+                });
+                gsap.to(span[videoId],{
+                  backgroundColor:'#afafaf'
+                })
+              }
+            }
+         })
+
+         if(videoId === 0){
+            anim.restart();
+         }
+       const animUpdate = () =>{
+      anim.progress(videoRef.current[videoId].currentTime / hightlightsSlides[videoId].videoDuration);
+     }
+
+     if(isPlaying){
+      gsap.ticker.add(animUpdate)
+     }else{
+      gsap.ticker.remove(animUpdate)
+     }
+   }
+ },[videoId , startPlay]);
+
+    const handleProcess = (type , i) => {
+        switch(type) {
+          case "video-end":
+            setvideo((pre)=>({
+              ...pre,isEnd:true,videoId:i+1
+            }))
+            break;
+
+            case 'video-last':
+              setvideo((pre)=>({
+                ...pre,isLastVideo:true
+              }))
+              break;
+
+              case 'video-reset':
+                setvideo((pre)=>({
+                  ...pre, isLastVideo:false,
+                  videoId:0
+                }))
+                break;
+
+                case 'play':
+                  setvideo((pre)=>({
+                    ...pre,isPlaying:!pre.isPlaying
+                  }))
+                  break;
