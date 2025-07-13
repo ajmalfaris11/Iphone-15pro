9af3d184ec4281e826e5f88e8e83ab865af6b1dc
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-13 14:06:00 +0530

    Add styles to ModelView features

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -14,3 +14,19 @@ const ModelView = ({index , groupRef , gsapType , controlRef , setRotationState 
     id={gsapType}
     className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''  } `}
     >
+     {/* Ambient Light */}
+     <ambientLight intensity={0.3} />
+
+     <PerspectiveCamera makeDefault position={[0,0,4]}/>
+
+     <Lights/>
+
+         <OrbitControls
+           makeDefault 
+           ref={controlRef}
+           enableZoom={false}
+           enablePan={false}
+           rotateSpeed={0.4}
+           target={new THREE.Vector3(0, 0, 0)}
+           onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+         />
