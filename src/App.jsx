58c9a340cbe761c77f4f1fbfa2c72c9cf37bdd38
ajmal faris <ajmalfaris.me@gmail.com>
2025-07-15 10:00:00 +0530

    Clean up unused imports and refs in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ import Features from './components/Features'
 import HowitWorks from './components/HowitWorks'
 import Footer from './components/Footer'
 import Lenis from '@studio-freight/lenis';
-import { useRef, useEffect } from 'react';
+import { useEffect } from 'react';
 import * as Sentry from "@sentry/react";
 
   
 const App = ()=> {
 
-  const lenisRef = useRef(null);
 
   useEffect(() => {
     const lenis = new Lenis({
@@ -21,8 +20,6 @@ const App = ()=> {
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
     });
-
-    lenisRef.current = lenis;
 
     const raf = (time) => {
       lenis.raf(time);
